2b2e7aa5da0c9f5b14ba3a02be85848827f066bf
nk888n <beearab999@gmail.com>
2026-09-05 19:08:46 -0400

    Use final checklist-complete admin client

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,2 +1,2 @@
-import AdminClient from "./AdminClient";
+import AdminClient from "./AdminClientFinal";
 export default function Admin(){return <main className="container"><nav className="nav"><a className="brand" href="/">VALE BEAUTY VK</a></nav><section className="section"><AdminClient/></section></main>}
