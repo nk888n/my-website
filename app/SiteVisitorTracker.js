1c5f2fea97762790a60525928ac6f96f6a4ca8d1
nk888n <beearab999@gmail.com>
2026-09-06 21:04:17 -0400

    Add client website visitor tracker

---
+++ app/SiteVisitorTracker.js
@@ -0,0 +1,13 @@
+"use client";
+import {useEffect} from "react";
+
+export default function SiteVisitorTracker(){
+  useEffect(()=>{
+    try{
+      if(sessionStorage.getItem("vale_visit_tracked"))return;
+      sessionStorage.setItem("vale_visit_tracked","1");
+    }catch{}
+    fetch("/api/analytics/visit",{method:"POST",credentials:"include",keepalive:true}).catch(()=>{});
+  },[]);
+  return null;
+}
